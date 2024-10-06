ba29f1ec527bed31c43c2d89da948b6ec4b61051
Alexei Tulloch <ajt254@cornell.edu>
2024-10-06 00:25:53 -0400

    enabled wallet connection

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -3,7 +3,8 @@ import Logo from "../logo.svg";
 import Eth from "../eth.svg";
 import { Link } from "react-router-dom";
 
-function Header() {
+function Header(props) {
+  const { address, isConnected, connect } = props;
   return (
     <header>
       <div className="leftH">
@@ -20,7 +21,11 @@ function Header() {
           <img src={Eth} alt="eth" className="eth" />
           Ethereum
         </div>
-        <div className="connectButton">Connect</div>
+        <div className="connectButton" onClick={connect}>
+          {isConnected
+            ? address.slice(0, 4) + "..." + address.slice(38)
+            : "Connect"}
+        </div>
       </div>
     </header>
   );
